fac4200cd905b35dff4b4ee8123cf66fda90152a
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-07-06 12:43:46 +0200

    Improve descriptions of characters

--- src/components/Game/Settings/CharactersDescriptions.js
+++ src/components/Game/Settings/CharactersDescriptions.js
@@ -1,9 +1,9 @@
 const characterDescritptions = {
-    PLO: "Zerowej nocy poznaję postać dowolnie wybranego gracza.", 
+    PLO: "Zerowej nocy poznaje postać dowolnie wybranego gracza.", 
     amorek: "Zerowej nocy łączy dwóch dowolnie wybranych graczy. Jeśli jeden z nich straci życie, to drugi również umiera .", 
     sędzia: "Decyduje, czy wyraża zgodę na pojedynek (czy dany pojedynek się odbędzie). Jeśli Sędzia nie żyje, zawsze jest zgoda na pojedynek.",
-    wariat: "W trakcie dnia może jedne raz zabić dowolnego gracza.",
-    komisarzCattani: "Każdej niezerowej nocy poznaje prawdziwą frakcję wybranego przez siebie frakcję.",
+    wariat: "W trakcie dnia raz na grę może zabić dowolnego gracza.",
+    komisarzCattani: "Każdej niezerowej nocy poznaje prawdziwą frakcję wybranego przez siebie gracza.",
     lekarz: "Każdej niezerowej nocy wybiera gracza, którego będzie leczyć, czyli chronić przed strzałem Mafii. Raz na grę może uleczyć samego siebie.",
     szybkiZMiasta: "Wygrywa pojedynki bez względu na wynik głosowania (wyjątkiem jest sytuacja, kiedy wszyscy wstrzymują się od głosu i nikt nie umiera oraz pojedynek z Szybkim z Mafii, w którym liczy się wynik głosowania).",
     szybkiZMafii: "Wygrywa pojedynki bez względu na wynik głosowania (wyjątkiem jest sytuacja, kiedy wszyscy wstrzymują się od głosu i nikt nie umiera oraz pojedynek z Szybkim z Miasta, w którym liczy się wynik głosowania). Hierarchia w Mafii - 3.",
